6863e2433af5c1e6b8d3cabd733feb5c884406d9
jcollyer <collyerdesign@gmail.com>
2015-04-22 07:41:44 -0700

    add sass watch in gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,18 @@
 var gulp = require('gulp');
+var sass = require('gulp-sass');
+var watch = require('gulp-watch');
 
 gulp.task('default', function() {
   // place code for your default task here
+});
+
+gulp.task('sass', function () {
+    gulp.src('./public/css/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./public/css'));
+});
+
+
+gulp.task('watch', function() {
+  gulp.watch('./public/css/*.scss', ['sass'])
 });
