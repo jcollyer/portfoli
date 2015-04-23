edc0981e1e04abfb9c2eec8843682f6d6f2a6462
jcollyer <collyerdesign@gmail.com>
2015-04-22 20:38:59 -0700

    add build out

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,1 +1,18 @@
+pagePlace = function() {
+  var pagePosition;
+  pagePosition = $("body").scrollTop() || pageYOffset;
+  if (pagePosition > 150) {
+    $("#navigation").addClass("plus");
+    $("#logo").attr("src","img/logo2.png");
+  } else {
+    $("#navigation").removeClass("plus");
+    $("#logo").attr("src","img/logo.png");
+  }
+};
 
+
+if ($(window).width() > 767) {
+  $(window).scroll(function() {
+    return pagePlace();
+  });
+};
